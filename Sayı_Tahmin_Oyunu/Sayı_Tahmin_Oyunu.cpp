5eea3f2e9841afc0a73571653910a766841b3781
Yusuf İşleyen <yusuf.isleyen99@gmail.com>
2025-05-30 18:45:08 +0300

    ilk versiyon

---
+++ Sayı_Tahmin_Oyunu/Sayı_Tahmin_Oyunu.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>  
+
+int main() {
+    std::srand(std::time(0));
+
+    char tekrarOyna;
+    int enIyiSkor = 9999;
+
+    do {
+        int maxSayi;
+        int maxDeneme = 9999;
+        int gizliSayi;
+        int denemeSayisi = 0;
+        int zorluk;
+
+        std::cout << "Zorluk seviyesi sec:\n";
+        std::cout << "1. Kolay (1-50)\n";
+        std::cout << "2. Orta (1-100)\n";
+        std::cout << "3. Zor (1-500, 5 hak)\n";
+        std::cout << "Secimin (1/2/3): ";
+        std::cin >> zorluk;
+
+        if (zorluk == 1) {
+            maxSayi = 50;
+        }
+        else if (zorluk == 2) {
+            maxSayi = 100;
+        }
+        else if (zorluk == 3) {
+            maxSayi = 500;
+            maxDeneme = 5;
+        }
+        else {
+            std::cout << "Gecersiz secim! Orta mod secildi.\n";
+            maxSayi = 100;
+        }
+
+        gizliSayi = std::rand() % maxSayi + 1;
+
+        std::cout << "1-" << maxSayi << " arasinda bir sayi tuttum. Basla bakalim!\n";
+        std::cin.ignore(); 
+
+        bool bildiMi = false;
+
+        while (denemeSayisi < maxDeneme) {
+            std::cout << "Tahminini gir (veya 'skor' yaz): ";
+            std::string girdi;
+            std::getline(std::cin, girdi);
+
+            if (girdi == "skor") {
+                std::cout << "Su ana kadarki en iyi skorun: " << enIyiSkor << " deneme\n";
+                continue; // tekrar sor tahmin
+            }
+
+            int tahmin;
+            try {
+                tahmin = std::stoi(girdi); 
+            }
+            catch (...) {
+                std::cout << "Gecersiz giris! Lutfen bir sayi yaz.\n";
+                continue;
+            }
+
+            denemeSayisi++;
+
+            if (tahmin < gizliSayi) {
+                std::cout << "Daha buyuk bir sayi gir.\n";
+            }
+            else if (tahmin > gizliSayi) {
+                std::cout << "Daha kucuk bir sayi gir.\n";
+            }
+            else {
+                std::cout << "Tebrikler! " << denemeSayisi << " denemede bildin!\n";
+                bildiMi = true;
+                if (denemeSayisi < enIyiSkor) {
+                    enIyiSkor = denemeSayisi;
+                    std::cout << "Yeni en iyi skor!\n";
+                }
+                break;
+            }
+
+            if (zorluk == 3 && denemeSayisi == maxDeneme) {
+                std::cout << "Uzgunum, hakkin bitti! Dogru cevap: " << gizliSayi << "\n";
+            }
+        }
+
+        std::cout << "Su ana kadarki en iyi skorun: " << enIyiSkor << " deneme\n";
+
+        std::cout << "Tekrar oynamak ister misin? (e/h): ";
+        std::cin >> tekrarOyna;
+        std::cin.ignore(); 
+
+    } while (tekrarOyna == 'e' || tekrarOyna == 'E');
+
+    std::cout << "Gorusuruz! Oyun bitti.\n";
+
+    return 0;
+}
